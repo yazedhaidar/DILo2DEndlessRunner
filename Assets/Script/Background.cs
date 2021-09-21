31f89c8d5f667aea91fb476aeda6e0a3d36b63be
yazedhaidar <88135665+yazedhaidar@users.noreply.github.com>
2021-09-21 21:18:48 +0700

    Update

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+   [Header("Background")]
+	public Renderer BG;
+	public float speedBG;
+
+	private float startPosX;
+	private Transform cameraMain;
+
+	void Start()
+	{
+        cameraMain = Camera.main.transform;
+		startPosX = cameraMain.position.x;
+		BG.sortingOrder = -5;
+	}
+
+	void Update()
+	{
+		float x = cameraMain.position.x - startPosX;
+
+		if (BG != null)
+		{
+			float offset = (x * speedBG) % 1;
+			BG.material.mainTextureOffset = new Vector2(offset, BG.material.mainTextureOffset.y);
+		}
+	}
+}
